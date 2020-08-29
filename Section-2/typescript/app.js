37873d9d15a3f38ea18c4374fd082e97dc11220b
Mario Ortega <mcor63@gmail.com>
2020-08-29 17:58:50 -0500

    It is added the section 2 with two exercises, types of data and template literal

---
+++ Section-2/typescript/app.js
@@ -0,0 +1,9 @@
+"use strict";
+(function () {
+    var NOMBRE = "Mario";
+    var APELLIDO = "Ortega";
+    var EDAD = 27;
+    // const SALIDA  = NOMBRE + " " + APELLIDO + " " + "( Edad: " + EDAD + ")";
+    var SALIDA = NOMBRE + " " + APELLIDO + " (" + EDAD + ")";
+    console.log(SALIDA);
+})();
---
+++ Section-2/typescript/typescript/template-literal.ts
@@ -0,0 +1,13 @@
+(function () {
+    const NOMBRE = "Mario";
+    const APELLIDO = "Ortega";
+    const EDAD = 27;
+
+    // const SALIDA  = NOMBRE + " " + APELLIDO + " " + "( Edad: " + EDAD + ")";
+
+    const SALIDA = `${NOMBRE} ${APELLIDO} (${EDAD})`;
+
+    console.log(SALIDA);
+})();
+
+
