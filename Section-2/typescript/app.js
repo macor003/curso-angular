b0fd5903919b65a4497dd862787f2e706acf01ff
Mario Ortega <mcor63@gmail.com>
2020-08-30 20:25:00 -0500

    promise practice

--- Section-2/typescript/app.js
+++ Section-2/typescript/app.js
@@ -1,3 +1,13 @@
 "use strict";
 (() => {
+    console.log('Inicio');
+    const prom1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('Se terminó el timeout');
+        }, 1000);
+    });
+    prom1
+        .then(mensaje => console.log(mensaje))
+        .catch(err => console.warn(err));
+    console.log('Fin');
 })();
