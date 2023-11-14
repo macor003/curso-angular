ff09009ef90c44c76f87655a2a5e95ae10f94bdc
Mario Ortega <mcor63@gmail.com>
2023-11-14 17:26:51 -0500

    Practica de Interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 // import './topics/01-basic-types.ts';
+import './topics/02-object-interface';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>HOLA MUNDO!</h1>  
