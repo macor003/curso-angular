6286ff6c9dba7d8fb8ccb1156de4e55490805d26
Mario Ortega <mcor63@gmail.com>
2023-11-15 22:44:40 -0500

    Se realiza primera practica con funciones en typescript

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,9 +1,10 @@
 import './style.css'
 // import './topics/01-basic-types.ts';
-import './topics/02-object-interface';
+// import './topics/02-object-interface';
+import './topics/03-functions';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>HOLA MUNDO!</h1>  
+  <h1>HOLA MARIO!</h1>  
 `
 
 console.log("Hola Mundo")
