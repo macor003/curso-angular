0115484bdac81a25efd7d9b8c706707e7bb13de8
Mario Ortega <mcor63@gmail.com>
2024-12-10 22:32:06 -0500

    Se realiza ejercicio de creación de componentes con comando ng y se implementa boostrap como estilos de la aplicación

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   template: `
   <p>Counter: {{counter}} </p>
 
-  <button (click)="incrementarBy(+1)">+1</button>
-  <button (click)="resetCounter()">reset</button>
-  <button (click)="incrementarBy(-1)">-1</button>
+  <button type="button" class="btn btn-primary btn-sm" (click)="incrementarBy(+1)">+1</button>
+  <button type="button" class="btn btn-danger btn-sm" (click)="resetCounter()">reset</button>
+  <button type="button" class="btn btn-success btn-sm" (click)="incrementarBy(-1)">-1</button>
 
   `
 })
