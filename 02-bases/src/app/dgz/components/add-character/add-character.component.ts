d3db5a568ad0971c3df7915c4e7368ea9a3fa1a3
Mario Ortega <mcor63@gmail.com>
2024-12-14 23:10:55 -0500

    Se agrega componente para formulario

---
+++ 02-bases/src/app/dgz/components/add-character/add-character.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrl: './add-character.component.css',
+  standalone: false
+})
+export class AddCharacterComponent { }
--- 02-bases/src/app/dgz/components/list/list.component.ts
+++ 02-bases/src/app/dgz/components/list/list.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-list',
-  imports: [],
-  template: `<p>list works!</p>`,
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
   styleUrl: './list.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false
 })
 export class ListComponent { }
