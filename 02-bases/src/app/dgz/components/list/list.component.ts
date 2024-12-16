b5f798be750dd48fd15b407f71a870365ae5759d
Mario Ortega <mcor63@gmail.com>
2024-12-15 23:44:59 -0500

    Se realiza primera practica con @Input y con ejemplo de uso de @NgFor

--- 02-bases/src/app/dgz/components/list/list.component.ts
+++ 02-bases/src/app/dgz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css',
   standalone: false
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[]=[];
+}
--- 02-bases/src/app/dgz/pages/main-page.component.ts
+++ 02-bases/src/app/dgz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dgz-main-page',
@@ -6,5 +7,12 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class MainPageComponent {
+
+  public characters: Character[] = [
+    { name: 'Goku', power: 9500 },
+    { name: 'Vegetta', power: 7500 },
+    { name: 'Krillin', power: 1000 },
+    { name: 'Trunks', power: 100 },
+  ]
 
 }
