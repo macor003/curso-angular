e4f2a4e2c209558d3de40f6a5ef851da5ef5b206
Mario Ortega <mcor63@gmail.com>
2025-01-03 23:52:33 -0500

    Se agrega uuid a cada item de la lista de characters

--- 02-bases/src/app/dgz/services/dbz.service.ts
+++ 02-bases/src/app/dgz/services/dbz.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+import { v4 as uuid } from 'uuid';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DbzService {
 
   public characters: Character[] = [
-    { name: 'Goku', power: 9500 },
-    { name: 'Vegetta', power: 7500 },
-    { name: 'Krillin', power: 1000 },
-    { name: 'Trunks', power: 100 }
+    { id: uuid(), name: 'Goku', power: 9500 },
+    { id: uuid(), name: 'Vegetta', power: 7500 },
+    { id: uuid(), name: 'Krillin', power: 1000 },
+    { id: uuid(), name: 'Trunks', power: 100 }
   ];
 
   onNewCharacter(character: Character): void {
-    this.characters.push(character);
-    console.log(character);
-    console.log(this.characters);
 
+    const newCharacter : Character = {id: uuid(), ...character};
+
+    this.characters.push(newCharacter);
   }
 
   onDeleteEvent(index: number): void {
