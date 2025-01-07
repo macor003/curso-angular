068cdfb139c764b64803a063789c841ea15cb81f
Mario Ortega <mcor63@gmail.com>
2025-01-06 22:57:23 -0500

    Se agrega home page component

---
+++ 03-gifs-app/src/app/gifs/pages/home/home.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
+  standalone: false
+})
+export class HomePageComponent { }
