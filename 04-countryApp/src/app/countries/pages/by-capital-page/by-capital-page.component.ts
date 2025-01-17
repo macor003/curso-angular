6a4d8e6679d1e1eaabaf7f45c28620c566537aed
Mario Ortega <mcor63@gmail.com>
2025-01-16 22:43:43 -0500

    Se agregan carpetas para carpeta countries, se crean pages necesarias y se configura el Routing para la sección.

---
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-by-capital-page',
+  standalone: false,
+
+  templateUrl: './by-capital-page.component.html',
+  styles: ``
+})
+export class ByCapitalPageComponent {
+
+}
