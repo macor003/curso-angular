fcfa4d9e7681390eb1cb8c7f2b34603e12ad2a22
Mario Ortega <mcor63@gmail.com>
2025-01-19 22:09:30 -0500

    Se actualiza tabla y se maneja error 404

--- 04-countryApp/src/app/countries/services/country.service.ts
+++ 04-countryApp/src/app/countries/services/country.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -11,7 +11,10 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapital(capital: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${capital}`);
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${capital}`)
+      .pipe(
+        catchError(error => of([]))
+      );
   }
 
 }
