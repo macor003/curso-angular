3376020729cea8010e91bb44354b8ca3cc6ea194
Mario Ortega <mcor63@gmail.com>
2025-01-27 21:56:25 -0500

    Se agregan botones al buscar por Region

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/country.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   standalone: false,
@@ -13,11 +15,18 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
 
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  public selectedRegion?: Region;
+
   public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) { }
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+
+    this.selectedRegion = region;
+
     this.isLoading = true;
     this.countriesService.searchRegion(region)
       .subscribe(response => {
